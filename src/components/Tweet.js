76beeec631705f2d9357d25e06bb57e731763fec
hannya <hanny.aly@gmail.com>
2020-09-26 11:46:25 -0400

    feat: Add pointer to parent tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,18 +4,27 @@ import { formatTweet } from '../utils/helpers'
 
 class Tweet extends Component {
     render() {
+        const { tweet, authedUser, id } = this.props
+        if (tweet === null) {
+            return <p>This tweet doesn't exist</p>
+        }
+
         return (
-            <div className='tweet'></div>
+            <div className='tweet'>{this.props.id}</div>
         )
     }
 }
 
-function mapStateToProps({authedUser, users, tweets}, { id }) {
+function mapStateToProps({users, authedUser, tweets}, { id }) {
     const tweet = tweets[id]
+    const parentTweet = tweet ? tweets[tweet.replyingTo] : null
 
     return {
         authedUser,
-        tweet: formatTweet(tweet, users[tweet.author], authedUser)
+        tweet: tweet 
+        ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+        : null
+
     }
 }
 
