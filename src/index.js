eb37f7a8e78e5d2400f96728005ecd9f823f9a6c
hannya <hanny.aly@gmail.com>
2020-09-26 00:07:50 -0400

    fix: Update react and react-redux b/c they were giving us error

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
-import { createStore } from "redux"
-import { Provider } from "react-redux"
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import reducer from "./reducers"
 import middleware from './middleware'
 
@@ -11,6 +11,6 @@ const store = createStore(reducer, middleware)
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <App />,
     </Provider>,   
 document.getElementById('root'))
